a25c2900e3671b57a631b97dcaa6a96a3ef122c5
yanhao gong <yanhao.gong@mail.mcgill.ca>
2020-02-03 16:41:10 -0500

    add task filtering by query

--- src/routers/task.js
+++ src/routers/task.js
@@ -21,15 +21,28 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
+// Get tasks
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
   try {
-    await req.user.populate('tasks').execPopulate();
+    await req.user
+      .populate({
+        path: 'tasks',
+        match
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
   }
 });
 
+// Get tasks by user id
 router.get('/tasks/:id', auth, async (req, res) => {
   const _id = req.params.id;
   try {
